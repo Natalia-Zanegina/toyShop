7d4ea7fc42c629fe626f98bef9f24834b163dfd5
NataliaZanegina <nataliazanegina@yandex.ru>
2024-07-05 13:34:29 +0300

    Initial commit

---
+++ Lottery.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Lottery {
+    ArrayList<Toy> toys;
+    static final String toyShopList = "toyShopList.bin";
+    static final String toyWinList = "toyWinList.bin";
+
+    public Lottery(int toysAmount) {
+        try {
+            System.out.println("Поиск базы данных");
+            this.toys = handleFile.readFromFile(toyShopList);
+            this.toys.addAll(createToyList(toysAmount));
+            System.out.println("Добавление новых данных");
+        } catch (Exception e) {
+            System.out.println("Создание новой базы данных");
+            this.toys = createToyList(toysAmount);
+        }
+    }
+
+    public void start() {
+        Toy prize = chooseToy(toys);
+        System.out.println("Выпала игрушка " + prize);
+        givePrize(prize);
+    }
+
+    public void stop() {
+        handleFile.saveToFile(toyShopList, toys);
+        System.out.println("База даных сохранена");
+    }
+
+
+    public Toy chooseToy(ArrayList<Toy> toys) {
+        int listSize = toys.size();
+        int randNum = new Random().nextInt(listSize);
+        Toy prize = toys.get(randNum);
+        toys.remove(randNum);
+        return prize;
+    }
+
+    public void givePrize(Toy prize){
+        try {
+            ArrayList<Toy> prizeList = handleFile.readFromFile(toyWinList);
+            prizeList.add(prize);
+            handleFile.saveToFile(toyWinList, prizeList);
+            System.out.println("Данные о выигрыше записаны в файл");
+        } catch (Exception e) {
+            handleFile.saveToFile(toyWinList, prize);
+            System.out.println("Создан новый файл. Данные добавлены");
+        }
+
+    }
+
+    public static ArrayList<Toy> createToyList(int amount) {
+        ArrayList<Toy> toys  = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            Toy toy = new Toy("toy" + i);
+            toys.add(toy);
+        }
+        handleFile.saveToFile(toyShopList, toys);
+        return toys;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Lottery lottery = new Lottery(10);
+        for (int i = 0; i < 5; i++) {
+            lottery.start();
+        }
+        lottery.stop();
+    }
+}
